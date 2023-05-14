a9e1868777eb8b10f2492e3f25a2831fe288ac13
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-05-14 15:08:56 +0530

    Api code update

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ button.addEventListener("click", async() => {
 
   if (firstName.length > 0 || lastName.length > 0 || email.length > 0 || message.length > 0) {
     try {
-      const fetchResponse = await fetch(
-        `https://www.grupverse.com/companywebsitemsg?name=${firstName} ${lastName}&email=email&msg=message&completed=false`, {
-          method: "POST"
-        }
-      );
+      let fd= new FormData();
+      fd.append('name', firstName+" "+lastName);
+      fd.append('email', email);
+      fd.append('msg', message);
+      fd.append('completed', false);
+      const fetchResponse = await fetch('https://www.grupverse.com/companywebsitemsg',{method:'POST',body:fd,});
 
       if (!fetchResponse.ok) {
         throw new Error('Error sending message');
