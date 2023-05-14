62b20f1923756ef16311fc75c8b9168e6d87b00d
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-05-14 09:08:21 +0530

    Index.js try snippet change

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 const button = document.getElementById("btn-contact");
 
-button.addEventListener("click", async () => {
+button.addEventListener("click", async() => {
   const firstName = document.getElementById("inputFirstName").value;
   const lastName = document.getElementById("inputLastName").value;
   const email = document.getElementById("inputEmail").value;
   const message = document.getElementById("message").value;
-  console.log("First Name: ", firstName);
-  console.log("Last Name: ", lastName);
+  console.log("FirstName: ", firstName);
+  console.log("LastName: ", lastName);
   console.log("Email: ", email);
   console.log("Message: ", message);
 
   if (firstName.length > 0 || lastName.length > 0 || email.length > 0 || message.length > 0) {
     try {
-      const fetchResponse = await fetch(
-        `https://www.grupverse.com/companywebsitemsg`, {
-          method: "POST",
+      const response = await fetch(
+        `https://www.grupverse.com/companywebsitemsg?name=${firstName} ${lastName}&email=email&msg=message&completed=false`, {
+          method: "POST"
+          //,
+          //   headers: {
+          //     "Content-Type": "application/x-www-form-urlencoded",
+          //   },
         }
       );
-
-      if (!fetchResponse.ok) {
-        throw new Error('Error sending message');
-      }
-
-      const serverResponse = await fetchResponse.json();
-      Swal.fire(
-        'Successful!',
-        'Your email has been received.',
-        'success'
-      )
 
     } catch (err) {
       console.log(err);
       Swal.fire(
         'Error',
-        'Something went wrong. Please try again later.',
+        'Something Went Wrong, Please try again later',
         'error'
       )
     }
