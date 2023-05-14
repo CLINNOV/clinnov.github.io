cfc42b8fbd9e63d8e103777da5f0922dbd9b4a7e
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-05-14 09:37:54 +0530

    Conatct us form and seetalert for that

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ button.addEventListener("click", async() => {
   const lastName = document.getElementById("inputLastName").value;
   const email = document.getElementById("inputEmail").value;
   const message = document.getElementById("message").value;
-  console.log("FirstName: ", firstName);
-  console.log("LastName: ", lastName);
-  console.log("Email: ", email);
-  console.log("Message: ", message);
+
 
   if (firstName.length > 0 || lastName.length > 0 || email.length > 0 || message.length > 0) {
     try {
       const response = await fetch(
         `https://www.grupverse.com/companywebsitemsg?name=${firstName} ${lastName}&email=email&msg=message&completed=false`, {
           method: "POST"
-          //,
-          //   headers: {
-          //     "Content-Type": "application/x-www-form-urlencoded",
-          //   },
         }
       );
 
+      if (!fetchResponse.ok) {
+        throw new Error('Error sending message');
+      }
+
+      Swal.fire(
+        'Successful!',
+        'Your email has been received.',
+        'success'
+      )
     } catch (err) {
       console.log(err);
       Swal.fire(
